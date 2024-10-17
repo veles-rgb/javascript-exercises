4735ddab444e700883797b69f6146a29d5a76a82
veles-rgb <181009546+veles-rgb@users.noreply.github.com>
2024-10-17 16:24:07 -0400

    Complete lesson 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let text = ""
+    if (num < 0) {
+        return "ERROR"
+    } else {
+        for (let i = 0; i < num; i++) {
+            text += string
+        }
+        return text
+    }
 };
 
 // Do not edit below this line

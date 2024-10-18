2d67bfcea1ad901ddd9da254745e1234e99c4c77
veles-rgb <181009546+veles-rgb@users.noreply.github.com>
2024-10-18 13:45:40 -0400

    Complete lesson 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reversed = splitString.reverse();
+    let joined = reversed.join("")
+    return joined;
 };
+
+reverseString("Hello")
 
 // Do not edit below this line
 module.exports = reverseString;

0b24d006aef917bb6a6e76cfcd1f34f024dea998
veles-rgb <181009546+veles-rgb@users.noreply.github.com>
2024-10-18 15:59:08 -0400

    Complete lesson 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (const arg of args) {
+        let index = array.indexOf(arg);
+        while (index > -1) {
+            array.splice(index, 1)
+            index = array.indexOf(arg)
+        }
+    }
+    return array
 };
 
 // Do not edit below this line

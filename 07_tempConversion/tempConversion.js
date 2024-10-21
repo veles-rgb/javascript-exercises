a5dd54990a888bd872603d4ebb6db3d95ed1bbda
veles-rgb <181009546+veles-rgb@users.noreply.github.com>
2024-10-21 16:44:30 -0400

    Complete lesson 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempf) {
+// x °F ≘ (x − 32) × ⁠5/9⁠ °C 
+let celsius = (tempf - 32) * (5/9);
+let celsiusRounded = Math.round(celsius * 10) / 10;
+return celsiusRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempc) {
+  // x °C ≘ (x × ⁠9/5⁠ + 32) °F 
+  let fahrenheit = (tempc * 9/5) + 32;
+  let fahrenheitRounded = Math.round(fahrenheit * 10) / 10;
+  return fahrenheitRounded;
 };
 
 // Do not edit below this line

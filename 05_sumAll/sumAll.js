5e0c916721d07d6d55201d0adbf6a4d835e23b7e
veles-rgb <181009546+veles-rgb@users.noreply.github.com>
2024-10-21 15:05:48 -0400

    Complete lesson 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    let finalSum = numTwo;
+    if (
+        numOne <= -1 ||
+        numTwo <= -1 ||
+        !Number.isInteger(numOne) ||
+        !Number.isInteger(numTwo)
+    ) {
+        return "ERROR"
+    } else if (numOne < numTwo) {
+        for (let i = numOne; i < numTwo; i++) {
+            finalSum = finalSum + i;
+        }
+        return finalSum;
+    } else if (numOne > numTwo) {
+        for (let i = numOne; i > numTwo; i--) {
+            finalSum = finalSum + i;
+        }
+        return finalSum;
+    }
 };
 
 // Do not edit below this line

99ebad8673786ec5ce8e28a18ef30ab4b5623260
veles-rgb <181009546+veles-rgb@users.noreply.github.com>
2024-11-07 16:37:51 -0500

    Complete lesson 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+};
 
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, current) => {
+        const oldestPerson = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath)
+        return oldestPerson < currentAge ? current : oldest
+    })
 };
 
 // Do not edit below this line
